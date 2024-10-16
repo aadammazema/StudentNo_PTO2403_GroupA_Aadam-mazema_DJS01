26c03cf868e1ffa0fb77d48543977c66cf38f2fa
Aadam Mazema <maze.aadam@gmail.com>
2024-10-16 17:26:38 +0200

    Refactored parameters
     Combined related values into a single object and use destructuring for better readability and easier management.

--- index.js
+++ index.js
@@ -6,13 +6,22 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+// const vel = 10000; // velocity (km/h)
+// const acc = 3; // acceleration (m/s^2)
+// const time = 3600; // seconds (1 hour)
+// const d = 0; // distance (km)
+// const fuel = 5000; // remaining fuel (kg)
+// const fbr = 0.5; // fuel burn rate (kg/s)
 
+// Given parameters
+const parameters = {
+  velocity: 10000, // velocity (km/h)
+  acceleration: 3, // acceleration (m/s^2)
+  time: 3600, // time in seconds (1 hour)
+  initialDistance: 0, // initial distance in km
+  remainingFuel: 5000, // remaining fuel in kg
+  fuelBurnRate: 0.5 // fuel burn rate in kg/s
+};
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
